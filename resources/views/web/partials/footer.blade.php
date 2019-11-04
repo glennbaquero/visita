b6cbd9d29aaac537136f1b309af19a0521142186
Jethro Hitosis <jethro@praxxys.ph>
2019-11-04 15:47:46 +0800

    Closes Issue #39 Front-end Header and Footer

---
+++ resources/views/web/partials/footer.blade.php
@@ -0,0 +1,17 @@
+<footer class="ftr-frm">
+	<div class="frm-cntnr align-c width--90">
+		<div class="inlineBlock-parent">
+			<div class="frm-cntnr__nav-col width--33 align-l">
+				<a href="">
+					<img src="{{ asset('images/visita-logo.png') }}" class="frm-cntnr__nav-logo">
+				</a>
+			</div
+			><div class="frm-cntnr__nav-col width--33 align-c frm-description clr--white">
+				<p>2019 &copy; {{ config('app.name') }}</p>
+			</div
+			><div class="frm-cntnr__nav-col width--33 align-r">
+				<a href="privacy-policy" class="frm-cntnr__nav-link bold">Privacy Policy</a>
+			</div>
+		</div>
+	</div>
+</footer>
---
+++ resources/views/web/partials/header.blade.php
@@ -0,0 +1,30 @@
+<header class="hdr-frm">
+	<div class="frm-cntnr align-c width--90">
+		<div class="inlineBlock-parent">
+			<div class="hdr-frm__nav-col">
+				<a href="" class="hdr-frm__nav-link">About Us</a>
+			</div
+			><div class="hdr-frm__nav-col">
+				<a href="" class="hdr-frm__nav-link">Alliance</a>
+			</div
+			><div class="hdr-frm__nav-col">
+				<a href="" class="hdr-frm__nav-link">Visita App</a>
+			</div
+			><div class="hdr-frm__nav-col">
+				<a href="">
+					<img src="{{ asset('images/visita-logo.png') }}" class="hdr-frm__nav-logo">
+				</a>
+			</div
+			><div class="hdr-frm__nav-col">
+				<a href="" class="hdr-frm__nav-link">Contact Us</a>
+			</div
+			><div class="hdr-frm__nav-col">
+				<a href="" class="hdr-frm__nav-link">Faq</a>
+			</div
+			><div class="hdr-frm__nav-col">
+				<a href="" class="hdr-frm__nav-link">Destinations</a>
+			</div
+			>
+		</div>
+	</div>
+</header>
