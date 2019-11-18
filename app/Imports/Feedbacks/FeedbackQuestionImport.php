5bebbd365ec1ac3bf9c5e1ffa4aba0ce99248003
KV Mocling <krizzia@praxxys.ph>
2019-11-18 17:06:16 +0800

    WIP Seeders

---
+++ app/Imports/Feedbacks/FeedbackQuestionImport.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Imports\Feedbacks;
+
+use Illuminate\Support\Collection;
+use Maatwebsite\Excel\Concerns\ToCollection;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+
+use App\Helpers\StringHelpers;
+
+use App\Models\Feedbacks\Feedback;
+
+class FeedbackQuestionImport implements ToCollection, WithHeadingRow
+{
+    public function collection(Collection $rows)
+    {
+    	foreach ($rows as $row) {
+    		Feedback::updateOrCreate([
+    			'question' => $row['question'],
+    		], [
+	            'answerable' => $row['answerable'],
+	            'show_other' => $row['show_other'],
+    		]);
+    	}
+    }
+}
