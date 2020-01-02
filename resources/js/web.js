15c2197b56bfb3afb3df7c8b54c4d94bda4d4348
Jethro Hitosis <jethro@praxxys.ph>
2020-01-02 12:01:48 +0800

    Vue JS form on booking

--- resources/js/web.js
+++ resources/js/web.js
@@ -3,6 +3,18 @@ Vue.component('dialog-container', require('./components/dialogs/DialogContainer.
 Vue.component('article-list', require('./views/web/articles/ArticleList.vue').default);
 Vue.component('selected-article', require('./views/web/articles/SelectedArticle.vue').default);
 
+/*
+* User Destination
+*/
 Vue.component('user-inquiry', require('./views/web/inquiries/Inquiry.vue').default);
 
+/*
+* User Destination
+*/
+
 Vue.component('user-destination', require('./views/web/destinations/Destination.vue').default);
+
+/*
+* User Booking
+*/
+Vue.component('user-booking', require('./views/web/bookings/Booking.vue').default);
