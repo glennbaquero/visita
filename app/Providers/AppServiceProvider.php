5e7a054fc7efceacbf353c1d4f0cf06f84d43baa
Glenn Carlo <baqueroglenncarlo@gmail.com>
2020-02-03 16:01:50 +0800

    fix issue in new page items

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,7 @@ use App\Helpers\AuthHelpers;
 use App\Helpers\EnvHelpers;
 use App\Helpers\GlobalChecker;
 
+use App\Models\Pages\PageItem;
 use App\Models\Books\Book;
 use App\Observers\BookingObserver;
 
@@ -38,6 +39,17 @@ class AppServiceProvider extends ServiceProvider
             
             View::share('checker', new GlobalChecker);
             View::share('env', new EnvHelpers);
+        });
+
+        View::composer('web.partials.footer', function($view) {
+            $twitter = PageItem::where('slug', 'twitter')->first();
+            $fb = PageItem::where('slug', 'facebook')->first();
+            $insta = PageItem::where('slug', 'instagram')->first();
+            $youtube = PageItem::where('slug', 'youtube')->first();
+            $view->with('fb', $fb);
+            $view->with('twitter', $twitter);
+            $view->with('insta', $insta);
+            $view->with('youtube', $youtube);
         });
     }
 
