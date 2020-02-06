e9e1c149d97bed95c3cc4a3c3f382638f2364edd
Glenn Carlo <baqueroglenncarlo@gmail.com>
2020-02-06 14:45:33 +0800

    WIP

--- resources/js/views/admin/bookings/version2/BookingsTable.vue
+++ resources/js/views/admin/bookings/version2/BookingsTable.vue
@@ -56,9 +56,9 @@
                     <td>{{ item.is_walkin }}</td>
                     <td>{{ item.status }}</td>
                     <td>{{ item.is_fullpayment }}</td>
-                    <td>{{ toMoney(parseFloat(item.initial_payment)) }}</td>
-                    <td>{{ toMoney(parseFloat(item.balance)) }}</td>
-                    <td>{{ toMoney(parseFloat(item.grand_total)) }}</td>
+                    <td>{{ withComma(item.initial_payment) }}</td>
+                    <td>{{ withComma(item.balance) }}</td>
+                    <td>{{ withComma(item.grand_total) }}</td>
                     <td>{{ item.payment_status }}</td>
                     <td>{{ item.created_at }}</td>
                     <td>
