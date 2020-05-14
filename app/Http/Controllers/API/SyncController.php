b7148d280527817c39f32e26a587328986e90e59
Glenn Carlo Baquero <glenn@praxxys.ph>
2020-05-14 09:36:40 +0800

    Fix issue in faqs

--- app/Http/Controllers/API/SyncController.php
+++ app/Http/Controllers/API/SyncController.php
@@ -67,6 +67,7 @@ class SyncController extends Controller
 						foreach ($remarks as $remark) {
 							// check if the remark has an id
 							if(!isset($remark->id)) {
+                                dd($remark);
 								$reservation->groupRemarks()->create([
 									'remark' => $remark['statement'],
 									'statement' => $remark['statement']
