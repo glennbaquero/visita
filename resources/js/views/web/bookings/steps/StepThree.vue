496f3c423b543d245d43c1c382af025a4f218fbd
Glenn Carlo Baquero <glenn@praxxys.ph>
2020-05-18 15:35:03 +0800

    Fix issue in step three

--- resources/js/views/web/bookings/steps/StepThree.vue
+++ resources/js/views/web/bookings/steps/StepThree.vue
@@ -212,7 +212,7 @@
 					for(var i = 0; i < guests.length; i++) {
 						if(guests[i].first_name != null) {
 					       	if(guests[i].special_fee_id != '0' || guests[i].spespecial_fee_id != 0) {
-					         	if(guests[i].paths != null) {
+					         	if(guests[i].paths != null || guests[i].paths.length) {
 							     	this.showNextButton = true;
 					        	} else {
 							     	this.showNextButton = false;
